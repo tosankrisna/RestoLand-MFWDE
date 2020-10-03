8240e7be035682fd9902dd96aeaf0d5da0ec2abd
tosankrisna <tosankrisna@gmail.com>
2020-10-04 00:56:13 +0800

    make integration testing for unliking a restaurant

---
+++ specs/unlikeRestaurantSpec.js
@@ -0,0 +1,56 @@
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurant-idb';
+
+const addLikeButtonContainer = () => {
+  document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+};
+
+describe('Unliking A Restaurant', () => {
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+  });
+
+  it('should display unlike widget when the restaurant has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this restaurant"]'))
+      .toBeTruthy();
+  });
+
+  it('should not display like widget when the restaurant has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this restaurant"]'))
+      .toBeFalsy();
+  });
+
+  it('should not throw error if the unliked restaurant is not in the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+
+    document.querySelector('[aria-label="unlike this restaurant"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestaurantIdb.getAllRestaurant()).toEqual([]);
+  })
+});
